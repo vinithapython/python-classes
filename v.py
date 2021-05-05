134af1ba846e85608607beba7edadd6a0504987f
unknown <vinitha@email.com>
2021-05-05 09:11:33 +0530

    hi

--- v.py
+++ v.py
@@ -1,10 +1,122 @@
-l = ["a","a","b","c","d","d"]
-d = {}
-for y in l:
-	# print(y)
-	if y in d:
-    d[y] and d[y] + 1
- print(y)
+
+# l = ["a","a","b","c","d","d"]
+# d = {}
+# for y in l:
+# 	# print(y)
+# 	if y in d:
+#     d[y] and d[y] + 1
+#  print(y)
 # else:
 # 	d[y] = 1
 # print(d)
+# l = ["a","a","b","c","d","d"]
+# d = {}
+# for y in l:
+# 	# print(y)
+# 	if y in d:
+#     d[y] and d[y] + 1
+#  print(y)
+# # else:
+# # 	d[y] = 1
+# # print(d)
+
+# def comment():
+# 	print("hi1")
+# comment()	
+
+# def up():
+# 	s="shiva"
+# 	return s[::-1]
+# print(up())	
+# def d2():
+# 	data=up()
+# 	return data
+# print(d2())
+# def do_something(name):
+# 	return"hello",name
+# print (do_something(7))	
+# def f(name):
+# 	v = "vins"
+# 	return v,name
+# def f2():
+# 	k = f("vins")
+# 	return(k)
+# print(f2())
+
+# def function(v,k):
+# 	return(v.intersection(k))
+# print(function({1,2,3},{1,3,4}))
+# def fun(s=10,a)
+#     return(s=10,a)
+#  fun()   
+# def fun(s=10,a=10):
+#     return(s,a)
+# fun()
+# def function(num=10):
+# 	print(num)
+# function(20)
+#write a function to print even numbers?
+# def evnnum(n):
+# 		for i in range(n):
+# 				if i % 2 == 0 :
+# 						print(i)
+# evnnum(100)
+ #hw							
+# def hw(n):
+# 	for i ,e in enumerate(a):
+# 		print(i,e)
+# a = ("shiva")
+# hw(enumerate)
+# function(5)
+# def function(v,k):
+# 	return(v.union(k))
+# print(function({0,1,2,3,4},{"shiva"}))
+# 05/05/2021
+# a = "shiva"
+# b = list (enumerate(a)) #method 1
+# print (b)
+
+# for x in enumerate("shiva"):   #method 2
+# 	print(x)
+
+# def multiple(a):
+# 	return 5 * a
+# print(multiple(1))
+
+# def multiple(k,v):
+# 	value = []
+# 	for d in range(1,k + 1):
+# 		value.append(d * v)
+# 	return value
+# print(multiple(10,5))
+
+# def multiple(a):
+# 	for x in range(1,5):
+# 		print(a,x, a * x)
+# a = 5
+# multiple(a)
+
+# list of comparsion
+# def multiple(n,s):
+# 	data = [d *s for d in range (1, n+1)]
+# 	return data
+# print (multiple(10,5))
+
+# def function(*args,**kwargs):
+# 		print(args, kwargs)
+# function(10,1,2, a=10, b=20)
+
+# def square(a):
+# 	v = []
+# 	for data in square:
+# 		v.append(data * data)
+#    print(v)
+# square([1,2,3,4])	
+
+# MAP
+def square(element):
+	return element * element
+data = list(map(square,[1,2,3,4]))
+print(data)	
+	
+			
